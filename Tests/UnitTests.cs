c637f17f62de3dbe0d1656c6495d250f5dd19e4d
wschmidt24 <111897204+wschmidt24@users.noreply.github.com>
2023-10-28 14:05:06 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -7,7 +7,7 @@ namespace GithubActionsLab
     public class Math
     {
         [Test]
-        public void Add_Valid()
+        public void Add_ValidSchmidt()
         {
             Assert.AreEqual(3, Program.Add("1", "2"));
             Assert.AreEqual(5, Program.Add("3", "2"));
@@ -15,7 +15,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Add_Invalid()
+        public void Add_InvalidSchmidt()
         {
             Assert.Throws<FormatException>(() => Program.Add("1", "a"));
             Assert.Throws<FormatException>(() => Program.Add("a", "1"));
@@ -23,7 +23,7 @@ namespace GithubActionsLab
         }
 
         [Test]
-        public void Add_Null()
+        public void Add_NullSchmidt()
         {
             Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
             Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
@@ -31,5 +31,100 @@ namespace GithubActionsLab
         }
 
         // Implement 3 tests per operation, following a similar pattern as above
+        [Test]
+        public void Subtract_ValidSchmidt()
+        {
+            Assert.AreEqual(1, Program.Add("3", "2"));
+            Assert.AreEqual(5, Program.Add("7", "2"));
+            Assert.AreEqual(11, Program.Add("18", "7"));
+        }
+
+        [Test]
+        public void Subtract_InvalidSchmidt()
+        {
+            Assert.Throws<FormatException>(() => Program.Add("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "a"));
+        }
+
+        [Test]
+        public void Subtract_NullSchmidt()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
+        }
+        
+        [Test]
+        public void Multiply_ValidSchmidt()
+        {
+            Assert.AreEqual(1, Program.Add("1", "1"));
+            Assert.AreEqual(55, Program.Add("11", "5"));
+            Assert.AreEqual(10, Program.Add("5", "2"));
+        }
+
+        [Test]
+        public void Multiply_InvalidSchmidt()
+        {
+            Assert.Throws<FormatException>(() => Program.Add("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "a"));
+        }
+
+        [Test]
+        public void Multiply_NullSchmidt()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
+        }
+
+        [Test]
+        public void Divide_ValidSchmidt()
+        {
+            Assert.AreEqual(1, Program.Add("3", "3"));
+            Assert.AreEqual(4, Program.Add("8", "2"));
+            Assert.AreEqual(7, Program.Add("77", "11"));
+        }
+
+        [Test]
+        public void Divide_InvalidSchmidt()
+        {
+            Assert.Throws<FormatException>(() => Program.Add("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "a"));
+        }
+
+        [Test]
+        public void Divide_NullSchmidt()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
+        }
+
+        [Test]
+        public void Power_ValidSchmidt()
+        {
+            Assert.AreEqual(9, Program.Add("3", "2"));
+            Assert.AreEqual(343, Program.Add("7", "3"));
+            Assert.AreEqual(128, Program.Add("2", "7"));
+        }
+
+        [Test]
+        public void Power_InvalidSchmidt()
+        {
+            Assert.Throws<FormatException>(() => Program.Add("1", "a"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "1"));
+            Assert.Throws<FormatException>(() => Program.Add("a", "a"));
+        }
+
+        [Test]
+        public void Power_NullSchmidt()
+        {
+            Assert.Throws<ArgumentNullException>(() => Program.Add("1", null));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, "1"));
+            Assert.Throws<ArgumentNullException>(() => Program.Add(null, null));
+        }
     }
 }
